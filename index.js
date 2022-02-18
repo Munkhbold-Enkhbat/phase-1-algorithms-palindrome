95fc975e06171f9282eb9a32484c879ba5bcc172
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-02-18 15:08:56 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function isPalindrome(word) {
   // Write your algorithm here
   let reversedWord = word.split('').reverse().join('')
-  return word === reversedWord
+  return word.toLowerCase() === reversedWord.toLowerCase()
 }
 
 /* 
