55f385fd14970ff3648ed131eeb2602b8dde84d6
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-02-18 17:58:35 -0800

    Optimized Version

--- index.js
+++ index.js
@@ -22,21 +22,42 @@ function isPalindrome(word) {
   compare it with the given string
   return true if palindrome, else false 
 */
-let myTest = str => isPalindrome(str) ? console.log(`${str} is a palindrome!`) : console.log(`${str} is not a palindrome!`)
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  myTest('madam')
-  myTest('maiden')
+  console.log("Expecting: true");
+  console.log("=> ", isPalindrome("a"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("Abba"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
+
+/*********** OPTIMIZED CODE ***********/
+
+// const reverse = str => {
+//   let rev = ''
+//   for(let i = str.length - 1; i >= 0; i--) {
+//     rev += str[i]
+//   }
+//   return rev
+// }
+
+// function isPalindrome(word) {
+//   // Write your algorithm here
+//   let reversedWord = reverse(word)
+//   return word.toLowerCase() === reversedWord.toLowerCase()
+// }
+
